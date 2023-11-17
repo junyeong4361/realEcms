88b4eacb2636e5f45f8ffd86e05aa87b5c353a66
jun0 <jun0dev@icloud.com>
2023-11-18 00:21:08 +0900

    feat : [002-SIGN-UP] 커스터머(고객)에 대한 Signup Service 구현

--- user-api/src/main/java/org/example/cms/user/domain/model/BaseEntity.java
+++ user-api/src/main/java/org/example/cms/user/domain/model/BaseEntity.java
@@ -12,7 +12,7 @@ import java.time.LocalDateTime;
 @Getter
 @MappedSuperclass
 @EntityListeners(value = {AuditingEntityListener.class})
-public class BaseEntity {
+public abstract class BaseEntity {
     @CreatedDate
     private LocalDateTime createdAt;
     @LastModifiedDate
